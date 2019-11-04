7267259c22d0facc02b146afdbad962b6a362b93
Daan Seegers <daan@raow.nl>
2019-11-04 15:22:12 +0100

    Replace Constructor in extended class

--- src/Helpers/Smushers/Resmush.php
+++ src/Helpers/Smushers/Resmush.php
@@ -2,10 +2,15 @@
 
 namespace OffbeatWP\ReSmush\Helpers\Smushers;
 
+use OffbeatWP\ReSmush\Helpers\General;
 use OffbeatWP\ReSmush\Helpers\Smushers\Base\SmushApi;
 
 class Resmush extends SmushApi
 {
-
+    public function __construct()
+    {
+        $this->url = 'http://api.resmush.it/?qlty=';
+        $this->exif = true;
+    }
 
 }
